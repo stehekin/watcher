8d49603139e4ad8a34a5f07be4f9c7fedc468ed5
qiang <qiangyu@gmail.com>
2025-03-23 16:14:30 +0000

    add streamline

--- src/signal/streamline/mod.rs
+++ src/signal/streamline/mod.rs
@@ -1,12 +1,38 @@
 use anyhow::Result;
-use tokio::sync::mpsc::UnboundedSender;
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 
-trait Handler {
-    type Item;
-
-    fn get_sender(&self) -> UnboundedSender<Self::Item>;
-    fn do_work(&self) -> Result<()>;
-    fn connect_to(&self, other: Self) -> Result<()>;
+struct Handler<T, H>
+where
+    T: prost::Message + 'static,
+    H: HandlerType<T>,
+{
+    sender: UnboundedSender<T>,
+    receiver: UnboundedReceiver<T>,
+    next: Option<UnboundedSender<T>>,
+    worker: H,
 }
 
-struct Streamline {}
+impl<T: prost::Message, H: HandlerType<T>> Handler<T, H> {
+    fn get_sender(&self) -> UnboundedSender<T> {
+        self.sender.clone()
+    }
+
+    pub(crate) fn connect<O: HandlerType<T>>(&mut self, other: Handler<T, O>) {
+        self.next = Some(other.get_sender());
+    }
+
+    pub(crate) async fn do_work(&mut self) -> Result<()> {
+        while let Some(mut message) = self.receiver.recv().await {
+            let message = self.worker.enrich(&mut message);
+            if let Some(next) = self.next.clone() {
+                next.send(message)?
+            }
+        }
+
+        anyhow::bail!("streamline closed")
+    }
+}
+
+trait HandlerType<T: prost::Message> {
+    fn enrich(&self, entity: &mut T) -> T;
+}
